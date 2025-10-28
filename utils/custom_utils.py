f2eb632e65e8048b586e964aa1c7feb6f87d79ea
roshanJayasekara <roshansampathmc41@gmail.com>
2025-10-28 14:18:07 +0000

    deduplication class

---
+++ utils/custom_utils.py
@@ -0,0 +1,16 @@
+from typing import List
+from pyspark.sql import DataFrame
+from pyspark.sql.window import Window
+from pyspark.sql.functions import concat, col, row_number
+
+class Transformations:
+
+
+    def dedup(self,df:DataFrame,dedup_col:List,cdc:str):
+
+        df = df.withColumn("dedupKey",concat(*dedup_col))
+        df = df.withColumn("dedupCounts",row_number().over(Window.partitionBy("dedupKey").orderBy(cdc)))
+        df = df.filter(col("dedupCounts") == 1)
+        df = df.drop("dedupKey","dedupCounts")
+
+        return df
